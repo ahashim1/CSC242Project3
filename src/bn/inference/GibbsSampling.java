885fc43531f9e99adf94c6d2aecc60eb2440aa64
Ali Hashim <ahashim@u.rochester.edu>
2018-03-26 12:30:39 -0400

    Started Gibbs Sampling

---
+++ src/bn/inference/GibbsSampling.java
@@ -0,0 +1,34 @@
+package bn.inference;
+
+import bn.core.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GibbsSampling {
+    public Distribution gibbsAsk(BayesianNetwork bn, RandomVariable X, Assignment e, int N){
+        int[] counts = new int[X.getDomain().size()];
+        List<RandomVariable> Z = getNonEvidenceVariables(bn, e);
+        Assignment x = e.copy();
+
+        for (int j=1; j<=N; j++){
+            for (RandomVariable Zi: Z){
+                
+            }
+        }
+    }
+
+    private List<RandomVariable> getNonEvidenceVariables(BayesianNetwork bn, Assignment e){
+        List<RandomVariable> vars = bn.getVariableList();
+        List<RandomVariable> nonEvidenceVars = new ArrayList<>();
+        for (RandomVariable X: vars){
+            if (e.containsKey(X)){
+                nonEvidenceVars.add(X);
+            }
+        }
+
+        return nonEvidenceVars;
+
+    }
+
+}
