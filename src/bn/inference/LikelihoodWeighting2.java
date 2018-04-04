763e831f0e79e59d5ce0b6516988538794a07fc4
Ali Hashim <ahashim@u.rochester.edu>
2018-04-04 13:20:01 -0400

    Yay Likelihood works

--- src/bn/inference/LikelihoodWeighting2.java
+++ src/bn/inference/LikelihoodWeighting2.java
@@ -7,73 +7,68 @@ import bn.parser.XMLBIFParser;
 import java.io.FileInputStream;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
-import java.util.ArrayList;
 
-public class RejectionSampling implements Inferencer {
-    int samples;
-    public RejectionSampling(int samples){
+public class LikelihoodWeighting2 implements Inferencer{
+    private int samples;
+
+    private class WeightedSample{
+        Assignment assignment;
+        Double weight;
+        private WeightedSample(Assignment e){
+            this.assignment = e;
+            this.weight = 1.0;
+        }
+    }
+    public LikelihoodWeighting2(int samples){
         this.samples = samples;
     }
 
+
     public Distribution ask(BayesianNetwork bn, RandomVariable X, Assignment e){
-
-        Distribution Q = new Distribution(X);
+        Distribution W = new Distribution(X);
         for (Object ob: X.getDomain()){
-            Q.put(ob, 0);
-        }
-        for (int j = 1; j <= samples; j++) {
-            Assignment event = priorSample(bn);
-
-            if (isConsistent(event, e)) {
-                double previous = Q.get(event.get(X));
-
-                Q.put(event.get(X), previous + 1);
-
-            }
+            W.put(ob, 0.0);
         }
 
-        Q.normalize();
-        return Q;
+        for (int j=1; j<=samples; j++){
+            WeightedSample weightedSample = Weighted_Sample(bn, e);
+
+            W.put(weightedSample.assignment.get(X), W.get(weightedSample.assignment.get(X)) + weightedSample.weight);
+        }
+
+        W.normalize();
+        return W;
+
     }
 
+    private WeightedSample Weighted_Sample(BayesianNetwork bn, Assignment e){
+        WeightedSample weightedSample = new WeightedSample(e.copy());
+        for (RandomVariable xi: bn.getVariableListTopologicallySorted()){
+            if (weightedSample.assignment.containsKey(xi)){
+                weightedSample.assignment.set(xi, weightedSample.assignment.get(xi));
+                weightedSample.weight *= bn.getProb(xi, weightedSample.assignment);
+            }else{
+                Distribution distribution = new Distribution(xi);
+                for (Object ob: xi.getDomain()){
+                    weightedSample.assignment.set(xi, ob);
+                    distribution.put(ob, bn.getProb(xi, weightedSample.assignment));
+                }
 
 
-    private Assignment priorSample(BayesianNetwork bn) {
-        Assignment event = new Assignment();
-        for (RandomVariable X: bn.getVariableListTopologicallySorted()){
+                weightedSample.assignment.set(xi, distribution.randomSample());
 
-            Distribution distribution = new Distribution(X);
-            for (Object ob: X.getDomain()){
-                event.set(X, ob);
-                distribution.put(ob, bn.getProb(X, event));
-                event.remove(X);
+
             }
-
-
-            event.set(X, distribution.randomSample());
         }
 
-        return event;
-    }
-
-    public boolean isConsistent(Assignment assignment, Assignment e){
-
-        for (Object ob: e.keySet()){
-            if (!e.get(ob).toString().equals(assignment.get(ob).toString())){
-                return false;
-            }
-
-        }
-
-        return true;
+        return weightedSample;
     }
 
     public static void main(String[] args) {
         String[] myargs = {"100000", "aima-alarm.xml", "B", "J", "true", "M", "true"};
 
         // wet grass example
-//        String[] myargs = {"100000", "aima-wet-grass.xml", "R", "S", "true"};
+//        String[] myargs = {"100000", "aima-wet-grass.xml", "R", "C", "true", "W", "true"};
 
         args = myargs;
 //        MAKE SURE I DELETE PREVIOUS TWO LINES BEFORE SUBMITTING
@@ -140,12 +135,13 @@ public class RejectionSampling implements Inferencer {
                 e.put(rv, evidenceVariables.get(key));
             }
 
-            RejectionSampling rejectionSampling = new RejectionSampling(samples);
-            Distribution distribution = rejectionSampling.ask(bn, query, e);
+            LikelihoodWeighting2 likelihoodWeighting2 = new LikelihoodWeighting2(samples);
+            Distribution distribution = likelihoodWeighting2.ask(bn, query, e);
             System.out.println(distribution.toString());
         } catch (Exception e) {
             e.printStackTrace();
         }
 
     }
+
 }
